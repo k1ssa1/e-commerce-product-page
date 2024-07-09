acdb4cb569b4a2832901b533e04a11361fa7c302
k1ssa1 <samikissai.edu@gmail.com>
2024-07-09 04:47:54 +0200

    feat: add basic hompage text

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,11 +4,21 @@ function Home() {
 
   const Home = styled.div`
     overflow: hidden;
+    width: 100%;
+    height: 50vh;
+    display: flex;
+  `
+
+  const HomeTitle = styled.h2`
+    margin: auto;
+    font-size: 6rem;
+    font-weight: 300;
+    color: hsl(220, 14%, 75%);
   `
 
   return (
     <Home>
-      
+      <HomeTitle>Homepage</HomeTitle>
     </Home>
   )
 }
